0f7c9143a27ecf6d609cd94c00ec1b244fcccc29
ShadyWolf-13 <134461735+ShadyWolf-13@users.noreply.github.com>
2023-05-27 01:38:53 -0300

    Aulas POO 01-07

--- POOJava/Animais/Cachorro.java
+++ POOJava/Animais/Cachorro.java
@@ -1,8 +1,84 @@
 package POOJava.Animais;
 
 public class Cachorro {
-    public String nome;
-    public String cor;
-    public int altura;
-    public double peso;
+
+    //Sequencia de escrita: Atributos, Construtores, Métodos
+
+//Atributos
+// Encapsulamento:
+// public : podem ser acessadas de qualquer pacote por qualquer um.
+// protected: acesso a mesma classe ou classes filhas (herança)
+// private: somente a classe pode acessar    
+    static int numeroDeCachorros;
+    private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private String estadoDeEspirito;
+
+//Construtores
+public Cachorro(){}
+
+public Cachorro(String nome, String cor, int altura, double peso, String estadoDeEspirito){
+    this.nome = nome;
+    this.cor = cor;
+    this.altura = altura;
+    this.peso = peso;
+    this.estadoDeEspirito = estadoDeEspirito;
+    numeroDeCachorros++;
+}
+
+//Métodos
+    public static int getNumeroDeCachorros(){
+        return numeroDeCachorros;
+    }
+    public static void setNumeroDeCachorros(int numeroDeCachorros){
+        Cachorro.numeroDeCachorros = numeroDeCachorros;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCor(){
+        return this.cor;
+    }
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+    
+    public int getAltura(){
+        return this.altura;
+    }
+    public void setAltura(int altura){
+        this.altura = altura;
+    }
+
+    public double getPeso(){
+        return this.peso;
+    }
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public void latir(){
+        System.out.println("AU AU");
+    }
+    
+    public String pegar(){
+        return "Bola";
+    }
+
+    public String interagir(String acao){
+        switch (acao) {
+            case "carinho": this.estadoDeEspirito = "feliz"; break;
+            case "vai dormir!": this.estadoDeEspirito = "bravo"; break;
+            case "pisar na patinha": this.estadoDeEspirito = "triste"; break;
+            default: this.estadoDeEspirito = "neutro"; 
+        }
+        return this.estadoDeEspirito;
+    } 
 }
--- POOJava/Classes.java
+++ POOJava/Classes.java
@@ -6,10 +6,41 @@ public class Classes {
     // Objeto pode ter características diferentes, mesmo sendo da mesma classe
     public static void main(String[] args) {
         Cachorro cachorro1 = new Cachorro();
-        cachorro1.nome = "Fantasma";
-        cachorro1.cor = "Branca";
-        cachorro1.altura = 45;
-        cachorro1.peso = 35.6;
+
+
+        Cachorro cachorro2 = new Cachorro("Luna", "Preta", 35, 25, "carinho");
+
+        System.out.println(cachorro2.getNumeroDeCachorros());
+
+        // Não é possível ter acesso a variáveis privadas
+        //cachorro1.nome = "Fantasma";
+        //cachorro1.cor = "Branca";
+        //cachorro1.altura = 45;
+        //cachorro1.peso = 35.6;
+        cachorro1.setNome("Fantasma");
+        cachorro1.setAltura(45);
+        cachorro1.setCor("Branco");
+        cachorro1.setPeso(36.5);
+
+        Cachorro cachorro3 = new Cachorro("Jack", "Branca", 35, 25, "");
+        
+        System.out.println(cachorro3.getNumeroDeCachorros());
+
         System.out.println(cachorro1);
-    }    
+        
+        //cachorro1.latir();
+        
+        //System.out.println("O cachorro pegou uma "+cachorro1.pegar());
+
+        System.out.println("O cachorro está "+cachorro1.interagir("carinho"));
+        System.out.println("O cachorro está "+cachorro1.interagir("nada"));
+        System.out.println("O cachorro está "+cachorro1.interagir("vai dormir!"));
+        System.out.println("O cachorro está "+cachorro1.interagir("pisar na patinha"));
+
+        System.out.println(cachorro2.getNome());
+        System.out.println(cachorro2.getPeso());
+
+        System.out.println(cachorro1.toString());
+        System.out.println(cachorro2.toString());
+    }   
 }
